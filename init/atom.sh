43b2284f04127039395a352518120db38b57addb
Warren Gavin <warren@dubelyoo.com>
2017-04-23 13:29:58 -0500

    Added.

---
+++ init/atom.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+apm install atom-beautify
+apm install busy-signal
+apm install linter-ui-default
+apm install git-plus
+apm install linter
+apm install linter-eslint
+apm install linter-php
