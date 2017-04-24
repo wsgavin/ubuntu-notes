4f917468f8dd9c0ec70b4ea63c6378804c052774
Warren Gavin <warren@dubelyoo.com>
2017-04-23 19:59:05 -0500

    Updated

--- init/nodejs.sh
+++ init/nodejs.sh
@@ -5,7 +5,7 @@
 echo
 echo "Installing nodejs..."
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.30.2/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
