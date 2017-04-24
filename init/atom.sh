9356023d6579f7796d6a185a6979d960258da8c2
Warren Gavin <warren@dubelyoo.com>
2017-04-23 21:25:14 -0500

    Updated

--- init/atom.sh
+++ init/atom.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Initializing atom with some packages..."
+
 apm install atom-beautify
 apm install busy-signal
 apm install intentions
@@ -9,3 +11,18 @@ apm install linter-eslint
 apm install linter-php
 apm install git-plus
 apm install linter-shellcheck
+
+echo "Configuring atom..."
+
+cat <<EOT >> ${HOME}/.atom/config.cson
+  core:
+    telemetryConsent: "no"
+  "markdown-preview":
+    useGitHubStyle: true
+  "tree-view":
+    hideIgnoredNames: true
+  welcome:
+    showOnStartup: false
+EOT
+
+echo "done."
