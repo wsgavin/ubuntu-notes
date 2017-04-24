3ef3ba557edbc49c78b4f12b59bf6336ec121d22
Warren Gavin <warren@dubelyoo.com>
2017-04-23 20:40:51 -0500

    Updated

--- init/gnome.sh
+++ init/gnome.sh
@@ -6,8 +6,8 @@ echo "Setting gnome-terminal preferences..."
 # Getting the terminal profile id.
 profile="$(dconf list /org/gnome/terminal/legacy/profiles:/)"
 
-dconf write /org/gnome/terminal/legacy/profiles:/${profile}/use-theme-colors "false"
-dconf write /org/gnome/terminal/legacy/profiles:/${profile}/background-color "'rgb(0,0,0)'"
-dconf write /org/gnome/terminal/legacy/profiles:/${profile}/foreground-color "'rgb(255,255,255)'"
+dconf write /org/gnome/terminal/legacy/profiles:/${profile}use-theme-colors "false"
+dconf write /org/gnome/terminal/legacy/profiles:/${profile}background-color "'rgb(0,0,0)'"
+dconf write /org/gnome/terminal/legacy/profiles:/${profile}foreground-color "'rgb(255,255,255)'"
 
 echo "done."
